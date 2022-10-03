3be7541ba2a324e2b805602212e9cf3625dbee0f
unknown <ibraheemhaseeb7@gmail.com>
2022-10-03 18:00:02 +0500

    added the feature to convert roman array to literal integer array

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,14 @@
 public class Driver {
     public static void main(String args[]) {
 
+        // roman object
         Roman test = new Roman();
 
+        // setting value for testing
         test.setRoman("XIV");
 
-        char[] array = test.createRomanArray();
+        for (int counter = 0; counter < 3; counter++) {
+            System.out.println(test.createIntArray().get(counter));
+        }
     }
 }
--- src/Roman.java
+++ src/Roman.java
@@ -1,26 +1,55 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Roman {
+    // attributes
     private String roman;
     Scanner input;
+    HashMap<Character, Integer> dict = new HashMap<Character, Integer>();
+    ArrayList<Integer> intArray = new ArrayList<Integer>();
 
+    // creating scanner to take input from the user
+    // adding the dataset for roman numerals
     {
         this.input = new Scanner(System.in);
+        dict.put('I', 1);
+        dict.put('V', 5);
+        dict.put('X', 10);
+        dict.put('L', 50);
+        dict.put('C', 100);
+        dict.put('D', 500);
+        dict.put('M', 1000);
     }
 
+    // to take input from the user if necessory
     public void takeRoman() {
         this.roman = input.nextLine();
     }
 
+    // setter
     public void setRoman(String roman) {
         this.roman = roman;
     }
 
+    // getter
     public String getRoman() {
         return this.roman;
     }
 
-    public char[] createRomanArray() {
+    // converting String into an array of characters
+    private char[] createRomanArray() {
         return this.roman.toCharArray();
+    }
+
+    public ArrayList<Integer> createIntArray() {
+
+        char[] array = createRomanArray();
+
+        for (char numeral : array) {
+            intArray.add(dict.get(numeral));
+        }
+
+        return this.intArray;
     }
 }
